30bdba74691410681b086b5a462ac34294621f84
NamrataSalgar427 <namratasalgar427@gmail.com>
2026-06-28 13:12:53 +0530

    Add HTML structure and CSS styling for Homepage

--- server.js
+++ server.js
@@ -1,9 +1,13 @@
 const express = require('express');
+const path=require('path');
 const app = express();
 const port = 3000;
 
+app.use(express.static('public'));
+
+
 app.get('/', (req, res) => {
-  res.send('Hello from my first server!');
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
 app.get('/about',(req,res)=>{
