35f24f3c360a2eedcd69404ea67aa05a4f3738d5
NamrataSalgar427 <namratasalgar427@gmail.com>
2026-06-28 09:35:48 +0530

    Add Express server with basic route

---
+++ server.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.get('/', (req, res) => {
+  res.send('Hello from my first server!');
+});
+
+app.get('/about',(req,res)=>{
+    res.send("This is the notes manager app");
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
